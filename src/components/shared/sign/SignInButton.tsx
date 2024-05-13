6b51e2ecb41ece582074efd311f06475857832b0
eunhak <akfmzh1207@naver.com>
2024-05-13 14:13:41 +0900

    fix: merge test

--- src/components/shared/sign/SignInButton.tsx
+++ src/components/shared/sign/SignInButton.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 const SignInButton = () => {
   const router = useRouter();
+  //
   return (
     <button
       className="w-[361px] h-12 px-3.5 rounded-lg border border-black justify-center items-center inline-flex text-black text-[15px] font-semibold font-pretendard leading-snug"
--- src/components/shared/sign/SignUpButton.tsx
+++ src/components/shared/sign/SignUpButton.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 const SignUpButton = () => {
   const router = useRouter();
+  //
   return (
     <button
       className="w-[361px] h-12 px-3.5 bg-space-black rounded-lg justify-center items-center inline-flex text-white text-[15px] font-semibold font-pretendard leading-snug"
