26ae3458f85da1afb5b399b6d9549a54425028f7
hojin <devbang5674@gmail.com>
2024-05-14 18:51:18 +0900

    fix: zustand 예시 코드 변경, store 폴더 경로 설정

--- src/store/example.store.ts
+++ src/store/example.store.ts
@@ -3,20 +3,68 @@ import { devtools, persist } from 'zustand/middleware';
 
 //기본 사용법 타입 에러는 일단 무시
 
-let userStore = (set) => ({
-  user: null,
-  setUser: (payload) =>
+let todoStore = (set) => ({
+  todos: [],
+  addTodo: (payload) =>
     set((state) => ({
-      user: payload.data
+      todos: [
+        ...state.todos,
+        {
+          text: payload.text,
+          isCompleted: payload.isCompleted
+        }
+      ]
+    })),
+
+  deleteTodo: (payload) =>
+    set((state) => ({
+      todos: state.todo.filter((todo) => todo.id !== payload.id)
+    })),
+
+  completeTodo: (payload) =>
+    set((state) => ({
+      todos: state.todos.map((todo) -> {
+        if(todo.id === payload.id){
+          return {
+            ...todo,
+            isComplete : true
+          }
+        }
+      })
     }))
 });
 
 // redux devtools처럼 chrome 확장자 앱 사용해서 사용가능
-userStore = devtools(userStore);
+todoStore = devtools(todoStore);
 
 // persist 새로고침시 데이터 유지
-userStore = persist(userStore, {
+todoStore = persist(todoStore, {
   name: 'userStore'
 });
 
-export const userStore = create(userStore);
+//store 완성 다른 작업 필요없이 갖다쓰면됨
+export const useTodoStore = create(todoStore);
+
+// 예시
+const {addTodo, deleteTodo ,completeTodo} = useTodoStore()
+
+const handleAddTodo = () => {
+  addTodo{
+    text : 'Add Todo',
+    isCompleted : false,
+    id: 1
+  }
+}
+
+const handleDeleteTodo = () => {
+  deleteTodo{
+    id : 1
+  }
+}
+
+const handleCompleteTodo = () => {
+  completeTodo{
+    text : 'Add Todo',
+    isCompleted : false,
+    id: 1
+  }
