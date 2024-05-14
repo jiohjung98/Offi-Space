baca0f3e70589320ecf721dd5333a12818c60800
hojin <devbang5674@gmail.com>
2024-05-14 15:51:26 +0900

    feat : community 폴더 구조 완성

---
+++ src/components/community/shared/CHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CHeader = () => {
+  return (
+    <div className="w-full flex gap-[16px] pl-[16px] pt-[8px] ">
+      <div className="cursor-pointer">커리어톡</div>
+      <div className="cursor-pointer">관심톡</div>
+    </div>
+  );
+};
+
+export default CHeader;
---
+++ src/pages/community/index.tsx
@@ -0,0 +1,13 @@
+import CHeader from '@/components/community/shared/CHeader';
+import MainContainer from '@/components/shared/MainContainer';
+import React from 'react';
+
+const CommunityPage = () => {
+  return (
+    <MainContainer>
+      <CHeader />
+    </MainContainer>
+  );
+};
+
+export default CommunityPage;
