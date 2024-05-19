600282262e61cf1c2cd4f3f33e6d648d3360831b
eunhak <akfmzh1207@naver.com>
2024-05-19 20:57:48 +0900

    feat: mypage 뼈대 구성

---
+++ src/pages/mypage/guide/index.tsx
@@ -0,0 +1,273 @@
+const GuidePage = () => {
+  return (
+    <div className="w-[393px] h-[1919px] relative bg-white">
+      <div className="w-[345px] h-[222px] left-[24px] top-[1558px] absolute bg-stone-50 rounded" />
+      <div className="w-[345px] h-48 left-[24px] top-[1068px] absolute bg-stone-50 rounded" />
+      <div className="w-[393px] h-16 pl-[11px] pr-[15.60px] pt-[17px] pb-[26px] left-0 top-0 absolute justify-start items-center gap-[235px] inline-flex">
+        <div className="grow shrink basis-0 self-stretch pt-px rounded-3xl justify-center items-center inline-flex">
+          <div className="w-[54px] h-5 text-center text-black text-[17px] font-bold font-['Pretendard'] leading-snug">
+            9:41
+          </div>
+        </div>
+        <div className="w-[77.40px] h-[13px] relative">
+          <div className="w-[27.40px] h-[13px] left-[50px] top-0 absolute flex-col justify-start items-start inline-flex" />
+          <img
+            className="w-[17px] h-[11.83px] left-[26px] top-[1px] absolute"
+            src="https://via.placeholder.com/17x12"
+          />
+        </div>
+      </div>
+      <div className="w-[393px] h-12 pl-4 pr-[253px] py-3 left-0 top-[64px] absolute bg-white justify-start items-center inline-flex">
+        <div className="justify-start items-center gap-3 inline-flex">
+          <div className="w-8 h-[19.20px] relative" />
+          <div className="text-center text-black/opacity-20 text-lg font-bold font-['Pretendard'] leading-normal">
+            이용 가이드
+          </div>
+        </div>
+      </div>
+      <div className="left-[16px] top-[132px] absolute text-black/opacity-20 text-lg font-bold font-['Pretendard'] leading-[27px]">
+        네트워크 이용 가이드
+      </div>
+      <div className="w-80 left-[57px] top-[203px] absolute text-neutral-600 text-sm font-normal font-['Pretendard'] leading-tight">
+        현재 층에 맞는 Offispace 와이파이에 접속해 인터넷을 사용하세요.
+        <br />
+        -네트워크 명: 지점 상세페이지 확인
+        <br />
+        -암호: 지점 상세페이지 확인
+      </div>
+      <div className="left-[58px] top-[347px] absolute text-neutral-600 text-sm font-normal font-['Pretendard'] leading-tight">
+        사무실 벽면 랜포트에 랜선을 연결해 인터넷을 사용하세요. <br />
+        추가 랜포트나 허브가 필요하다면 매니저에게 문의하세요.
+      </div>
+      <div className="w-[321px] left-[56px] top-[451px] absolute text-neutral-600 text-sm font-normal font-['Pretendard'] leading-tight">
+        공인/사설 IP 신청이 필요하거나 기타 네트워크 인프라에 <br />
+        관한 문의가 필요하다면 1:1 문의를 사용해주세요.
+      </div>
+      <div className="left-[16px] top-[171px] absolute justify-start items-start gap-4 inline-flex">
+        <div className="w-6 h-6 relative">
+          <div className="w-6 h-6 left-0 top-0 absolute bg-violet-100 rounded-full" />
+          <div className="w-4 h-[11.75px] left-[4px] top-[7.13px] absolute">
+            <div className="w-[2.50px] h-[2.50px] left-[6.74px] top-[9.39px] absolute bg-slate-500 rounded-full" />
+          </div>
+        </div>
+        <div className="text-neutral-700 text-base font-semibold font-['Pretendard'] leading-normal">
+          무선 인터넷
+        </div>
+      </div>
+      <div className="left-[16px] top-[315px] absolute justify-start items-start gap-4 inline-flex">
+        <div className="w-6 h-6 relative">
+          <div className="w-6 h-6 left-0 top-0 absolute bg-violet-100 rounded-full" />
+          <div className="w-[14.40px] h-[14.40px] left-[4.80px] top-[4.80px] absolute" />
+        </div>
+        <div className="text-neutral-700 text-base font-semibold font-['Pretendard'] leading-normal">
+          유선 인터넷
+        </div>
+      </div>
+      <div className="left-[16px] top-[419px] absolute justify-start items-start gap-4 inline-flex">
+        <div className="w-6 h-6 relative">
+          <div className="w-6 h-6 left-0 top-0 absolute bg-violet-100 rounded-full" />
+          <div className="w-[13.60px] h-[12.80px] left-[5.20px] top-[5.60px] absolute">
+            <div className="w-[3.41px] h-[2.98px] left-[5.29px] top-[3.18px] absolute"></div>
+          </div>
+        </div>
+        <div className="text-neutral-700 text-base font-semibold font-['Pretendard'] leading-normal">
+          ip신청
+        </div>
+      </div>
+      <div className="w-[393px] h-1 left-0 top-[523px] absolute bg-neutral-200" />
+      <div className="left-[16px] top-[559px] absolute text-black/opacity-20 text-lg font-bold font-['Pretendard'] leading-[27px]">
+        공간 예약 가이드
+      </div>
+      <div className="left-[16px] top-[1328.34px] absolute text-black/opacity-20 text-lg font-bold font-['Pretendard'] leading-[27px]">
+        방문객 초대 가이드
+      </div>
+      <div className="left-[39px] top-[1509.84px] absolute text-center text-indigo-800 text-base font-medium font-['Pretendard'] leading-normal">
+        사전신청
+      </div>
+      <div className="left-[181px] top-[1509.84px] absolute text-center text-indigo-800 text-base font-medium font-['Pretendard'] leading-normal">
+        승인
+      </div>
+      <div className="left-[312px] top-[1509.76px] absolute text-center text-indigo-800 text-base font-medium font-['Pretendard'] leading-normal">
+        출입
+      </div>
+      <div className="w-[361px] left-[16px] top-[594px] absolute text-neutral-600 text-sm font-normal font-['Pretendard'] leading-tight">
+        Offispace에는 목적에 맞게 사용할 수 있는 다양한 공간이 준비되어 있습니다. 앱 및
+        웹페이지의 예약 메뉴를 통해 다양한 공간을 이용해보세요.
+      </div>
+      <div className="w-[317px] left-[38px] top-[1579px] absolute">
+        <span className="text-neutral-700 text-sm font-semibold font-['Pretendard'] leading-tight">
+          Step1. <br />
+        </span>
+        <span className="text-stone-500 text-sm font-normal font-['Pretendard'] leading-tight">
+          사전 신청을 통해 Offispace에 방문객을 초대해보세요!
+          <br />
+        </span>
+        <span className="text-neutral-700 text-sm font-normal font-['Pretendard'] leading-tight">
+          <br />
+        </span>
+        <span className="text-neutral-700 text-sm font-semibold font-['Pretendard'] leading-tight">
+          Step2. <br />
+        </span>
+        <span className="text-stone-500 text-sm font-normal font-['Pretendard'] leading-tight">
+          방문객 초대 전 각 지점의 매니저에게 방문객 명수, 방문 예정 시간을 알리고 사전
+          승인을 받아야 합니다. <br />
+        </span>
+        <span className="text-neutral-700 text-sm font-normal font-['Pretendard'] leading-tight">
+          <br />
+        </span>
+        <span className="text-neutral-700 text-sm font-semibold font-['Pretendard'] leading-tight">
+          Step3. <br />
+        </span>
+        <span className="text-stone-500 text-sm font-normal font-['Pretendard'] leading-tight">
+          보안을 위해 사전 승인을 받은 방문객만 출입가능합니다.
+        </span>
+      </div>
+      <div className="w-[393px] h-1 left-0 top-[1292px] absolute bg-neutral-200" />
+      <div className="left-[23px] top-[686px] absolute flex-col justify-start items-center gap-1 inline-flex">
+        <div className="w-14 h-14 relative">
+          <div className="w-14 h-14 left-0 top-0 absolute bg-violet-100 rounded-full" />
+          <div className="w-[31.43px] h-8 pl-[1.77px] pr-[2.58px] left-[12.29px] top-[11.99px] absolute flex-col justify-center items-center inline-flex">
+            <div className="w-[27.09px] h-[32.23px] relative"></div>
+          </div>
+        </div>
+        <div className="text-indigo-700 text-sm font-medium font-['Pretendard'] leading-[21px]">
+          미팅룸
+        </div>
+      </div>
+      <div className="w-[259px] h-10 left-[105px] top-[686px] absolute text-neutral-600 text-sm font-normal font-['Pretendard'] leading-[21px]">
+        3~15인까지 사용할 수 있는 다양한 크기의 미팅룸이 있습니다.
+      </div>
+      <div className="w-0.5 h-0.5 left-[99px] top-[747px] absolute bg-indigo-700 rounded-full" />
+      <div className="w-0.5 h-0.5 left-[99px] top-[873px] absolute bg-indigo-700 rounded-full" />
+      <div className="w-0.5 h-0.5 left-[99px] top-[968.91px] absolute bg-indigo-700 rounded-full" />
+      <div className="w-0.5 h-0.5 left-[99px] top-[827px] absolute bg-indigo-700 rounded-full" />
+      <div className="w-0.5 h-0.5 left-[99px] top-[694px] absolute bg-indigo-700 rounded-full" />
+      <div className="w-[259px] h-10 left-[105px] top-[738px] absolute text-neutral-600 text-sm font-normal font-['Pretendard'] leading-[21px]">
+        비품 등 공간에 대한 정보는 공간 상세 페이지를 통해 확인하세요.
+      </div>
+      <div className="left-[23px] top-[817px] absolute flex-col justify-start items-center gap-1 inline-flex">
+        <div className="w-14 h-14 relative">
+          <div className="w-14 h-14 left-0 top-0 absolute bg-violet-100 rounded-full" />
+          <div className="w-[23.67px] h-[25.80px] left-[16px] top-[15px] absolute" />
+        </div>
+        <div className="text-indigo-700 text-sm font-medium font-['Pretendard'] leading-[21px]">
+          리차징룸
+        </div>
+      </div>
+      <div className="w-[259px] h-10 left-[105px] top-[817px] absolute text-neutral-600 text-sm font-normal font-['Pretendard'] leading-[21px]">
+        업무 중 충전이 필요할 때 사용할 수 있는 공간이 있습니다.
+      </div>
+      <div className="w-[259px] h-10 left-[105px] top-[865px] absolute text-neutral-600 text-sm font-normal font-['Pretendard'] leading-[21px]">
+        지점 별로 운영 여부가 상이하니 확인 후 예약을 진행해주세요.
+      </div>
+      <div className="left-[23px] top-[948px] absolute flex-col justify-start items-center gap-1 inline-flex">
+        <div className="w-14 h-14 relative">
+          <div className="w-14 h-14 left-0 top-0 absolute bg-violet-100 rounded-full" />
+          <div className="w-[21.96px] h-[26.49px] left-[17.02px] top-[14.76px] absolute flex-col justify-center items-center inline-flex">
+            <div className="w-[21.96px] h-[26.49px] relative"></div>
+          </div>
+        </div>
+        <div className="text-indigo-700 text-sm font-medium font-['Pretendard'] leading-[21px]">
+          포커스룸
+        </div>
+      </div>
+      <div className="w-[258px] h-10 left-[105px] top-[958px] absolute text-neutral-600 text-sm font-normal font-['Pretendard'] leading-[21px]">
+        파티션으로 분리된 조용한 환경에서 업무에 몰입해보세요.
+      </div>
+      <div className="w-[316px] left-[38px] top-[1088px] absolute">
+        <span className="text-neutral-600 text-sm font-bold font-['Pretendard'] leading-tight">
+          이용 Tip! <br />
+        </span>
+        <span className="text-neutral-600 text-sm font-normal font-['Pretendard'] leading-tight">
+          <br />
+          음식 섭취는?
+          <br />
+          모두를 위해 취식이 가능한 공간에서만 해주세요.
+          <br />
+        </span>
+        <span className="text-neutral-600 text-sm font-bold font-['Pretendard'] leading-tight">
+          <br />
+        </span>
+        <span className="text-neutral-600 text-sm font-normal font-['Pretendard'] leading-tight">
+          이용 종료 후에는?
+          <br />
+          다음 이용자를 위해 꼭 이용 종료 버튼을 눌러주세요.
+        </span>
+      </div>
+      <div className="left-[47px] top-[1491.68px] absolute text-center text-neutral-700 text-sm font-semibold font-['Pretendard'] leading-[21px]">
+        Step 1
+      </div>
+      <div className="left-[174px] top-[1491.68px] absolute text-center text-neutral-700 text-sm font-semibold font-['Pretendard'] leading-[21px]">
+        Step 2
+      </div>
+      <div className="left-[305px] top-[1491.59px] absolute text-center text-neutral-700 text-sm font-semibold font-['Pretendard'] leading-[21px]">
+        Step 3
+      </div>
+      <div className="w-[393px] pl-5 pr-6 py-3 left-0 top-[1853px] absolute bg-white border-t border-zinc-100 justify-center items-end gap-[45px] inline-flex">
+        <div className="flex-col justify-start items-center gap-1 inline-flex">
+          <div className="w-[35.64px] h-6 relative">
+            <img
+              className="w-6 h-6 left-[5.82px] top-0 absolute"
+              src="https://via.placeholder.com/24x24"
+            />
+          </div>
+          <div className="text-center text-neutral-600 text-xs font-normal font-['Pretendard']">
+            홈
+          </div>
+        </div>
+        <div className="flex-col justify-start items-center gap-1 inline-flex">
+          <div className="w-[35.64px] h-6 pl-[4.80px] pr-[4.98px] justify-center items-center inline-flex">
+            <div className="w-[25.86px] h-[26.40px] relative">
+              <div className="w-[25.86px] h-[22.63px] left-[-0px] top-[3.77px] absolute rounded-sm border border-neutral-700" />
+              <div className="w-[1.08px] h-[8.62px] left-[4.31px] top-[-0px] absolute bg-neutral-700" />
+              <div className="w-[1.08px] h-[8.62px] left-[21.01px] top-[-0px] absolute bg-neutral-700" />
+            </div>
+          </div>
+          <div className="text-center text-neutral-600 text-xs font-normal font-['Pretendard']">
+            예약
+          </div>
+        </div>
+        <div className="flex-col justify-start items-center gap-1 inline-flex">
+          <div className="w-[24.27px] h-6 relative" />
+          <div className="text-center text-neutral-600 text-xs font-normal font-['Pretendard']">
+            내 주변
+          </div>
+        </div>
+        <div className="flex-col justify-start items-center gap-1 inline-flex">
+          <div className="w-[20.40px] h-6 relative">
+            <div className="w-[16.20px] h-3 left-[4.20px] top-[12px] absolute rounded-sm border border-neutral-700" />
+            <div className="w-[18px] h-6 left-0 top-0 absolute bg-white rounded-tl-sm rounded-tr-sm rounded-bl-sm border border-neutral-700" />
+            <div className="w-[3.60px] h-3 left-[16.80px] top-[12px] absolute rounded-tr-sm rounded-bl-sm rounded-br-sm border border-neutral-700" />
+          </div>
+          <div className="text-center text-neutral-600 text-xs font-normal font-['Pretendard']">
+            커뮤니티
+          </div>
+        </div>
+        <div className="flex-col justify-start items-center gap-1 inline-flex">
+          <div className="w-6 h-6 relative" />
+          <div className="text-center text-neutral-600 text-xs font-normal font-['Pretendard']">
+            마이
+          </div>
+        </div>
+      </div>
+      <div className="w-[88px] h-[88px] left-[23px] top-[1395px] absolute bg-violet-100 rounded-full" />
+      <div className="w-[33.66px] h-[38.18px] left-[50.17px] top-[1420.60px] absolute">
+        <div className="w-[14.10px] h-[15.25px] left-[9.77px] top-[4.57px] absolute"></div>
+        <div className="w-[22.71px] h-[7.29px] left-[5.47px] top-[26.02px] absolute"></div>
+      </div>
+      <div className="w-[88px] h-[88px] left-[151px] top-[1395px] absolute bg-violet-100 rounded-full" />
+      <div className="w-[48.33px] h-[47.72px] left-[173.59px] top-[1416.15px] absolute">
+        <div className="w-[14.10px] h-[15.25px] left-[13.98px] top-[9.34px] absolute"></div>
+        <div className="w-[22.71px] h-[7.29px] left-[9.68px] top-[30.80px] absolute"></div>
+        <div className="w-5 h-5 left-[28.33px] top-[24.53px] absolute"></div>
+      </div>
+      <div className="w-[88px] h-[88px] left-[282px] top-[1395px] absolute bg-violet-100 rounded-full" />
+      <div className="w-[39.25px] h-[37.35px] left-[308.75px] top-[1421.74px] absolute">
+        <div className="w-[30.26px] h-[32.70px] left-0 top-0 absolute"></div>
+        <div className="w-5 h-5 left-[19.25px] top-[17.35px] absolute"></div>
+      </div>
+    </div>
+  );
+};
+
+export default GuidePage;
---
+++ src/pages/mypage/withdraw/index.tsx
@@ -0,0 +1,110 @@
+const withDrawPage = () => {
+  return (
+    <div className="w-[393px] h-[852px] relative bg-white">
+      <div className="w-[393px] h-16 pl-[11px] pr-[15.60px] pt-[17px] pb-[26px] left-0 top-0 absolute justify-start items-center gap-[235px] inline-flex">
+        <div className="grow shrink basis-0 self-stretch pt-px rounded-3xl justify-center items-center inline-flex">
+          <div className="w-[54px] h-5 text-center text-black text-[17px] font-bold font-['Pretendard'] leading-snug">
+            9:41
+          </div>
+        </div>
+        <div className="w-[77.40px] h-[13px] relative">
+          <div className="w-[27.40px] h-[13px] left-[50px] top-0 absolute flex-col justify-start items-start inline-flex" />
+          <img
+            className="w-[17px] h-[11.83px] left-[26px] top-[1px] absolute"
+            src="https://via.placeholder.com/17x12"
+          />
+        </div>
+      </div>
+      <div className="w-[393px] h-12 pl-4 pr-[271px] py-3 left-0 top-[64px] absolute bg-white justify-start items-center inline-flex">
+        <div className="justify-start items-center gap-3 inline-flex">
+          <div className="w-8 h-[19.20px] relative" />
+          <div className="text-center text-black text-lg font-bold font-['Pretendard'] leading-normal">
+            회원탈퇴
+          </div>
+        </div>
+      </div>
+      <div className="w-[361px] h-12 px-3.5 left-[16px] top-[616px] absolute bg-zinc-400 rounded-lg justify-center items-center inline-flex">
+        <div className="px-7 justify-center items-center gap-2 flex">
+          <div className="text-white/opacity-90 text-[15px] font-semibold font-['Pretendard'] leading-snug">
+            회원탈퇴
+          </div>
+        </div>
+      </div>
+      <div className="left-[16px] top-[132px] absolute text-black text-[22px] font-semibold font-['Pretendard'] leading-[30.80px]">
+        떠나시게 되어 아쉬워요.
+        <br />
+        유의 사항을 확인해주세요.
+      </div>
+      <div className="left-[31.98px] top-[264.21px] absolute text-indigo-700 text-sm font-bold font-['Pretendard'] leading-[21px]">
+        회원 탈퇴 시 유의 사항
+      </div>
+      <div className="w-[361px] h-[227px] left-[16px] top-[250px] absolute border border-neutral-200" />
+      <div className="w-[332px] left-[32px] top-[317px] absolute text-stone-500 text-sm font-normal font-['Pretendard'] leading-[15px]">
+        ・회원 탈퇴 시 개인정보는 삭제되고 복구되지 않습니다.
+        <br />
+        <br />
+        ・동일 이메일로 재가입 시 인증 절차를 다시 진행해야 합니다.
+        <br />
+        <br />
+        ・커뮤니티 내에 작성하셨던 글과 댓글은 삭제할 수 없습니다. (추후 동일 이메일로
+        재가입 시에도 삭제 불가능)
+        <br />
+        <br />
+        ・예약된 내역은 모두 예약 취소 처리됩니다. (참여자일 경우 참여자에서만 제외)
+      </div>
+      <div className="left-[79.81px] top-[515px] absolute text-stone-500 text-sm font-normal font-['Pretendard'] leading-[21px]">
+        안내 사항을 모두 확인하였으며, 이에 동의합니다.
+      </div>
+      <div className="w-5 h-5 left-[50.30px] top-[515px] absolute rounded-sm border border-neutral-600" />
+      <div className="w-[393px] pl-5 pr-6 py-3 left-0 top-[786px] absolute bg-white border-t border-zinc-100 justify-start items-start gap-[45px] inline-flex">
+        <div className="flex-col justify-start items-center gap-1 inline-flex">
+          <div className="w-[35.64px] h-6 relative">
+            <img
+              className="w-6 h-6 left-[5.82px] top-0 absolute"
+              src="https://via.placeholder.com/24x24"
+            />
+          </div>
+          <div className="text-center text-neutral-600 text-xs font-normal font-['Pretendard']">
+            홈
+          </div>
+        </div>
+        <div className="flex-col justify-start items-center gap-1 inline-flex">
+          <div className="w-[35.64px] h-6 pl-[4.80px] pr-[4.98px] justify-center items-center inline-flex">
+            <div className="w-[25.86px] h-[26.40px] relative">
+              <div className="w-[25.86px] h-[22.63px] left-[-0px] top-[3.77px] absolute rounded-sm border border-neutral-700" />
+              <div className="w-[1.08px] h-[8.62px] left-[4.31px] top-[-0px] absolute bg-neutral-700" />
+              <div className="w-[1.08px] h-[8.62px] left-[21.01px] top-[-0px] absolute bg-neutral-700" />
+            </div>
+          </div>
+          <div className="text-center text-neutral-600 text-xs font-normal font-['Pretendard']">
+            예약
+          </div>
+        </div>
+        <div className="flex-col justify-start items-center gap-1 inline-flex">
+          <div className="w-[24.27px] h-6 relative" />
+          <div className="text-center text-neutral-600 text-xs font-normal font-['Pretendard']">
+            내 주변
+          </div>
+        </div>
+        <div className="flex-col justify-start items-center gap-1 inline-flex">
+          <div className="w-[20.40px] h-6 relative">
+            <div className="w-[16.20px] h-3 left-[4.20px] top-[12px] absolute rounded-sm border border-neutral-700" />
+            <div className="w-[18px] h-6 left-0 top-0 absolute bg-white rounded-tl-sm rounded-tr-sm rounded-bl-sm border border-neutral-700" />
+            <div className="w-[3.60px] h-3 left-[16.80px] top-[12px] absolute rounded-tr-sm rounded-bl-sm rounded-br-sm border border-neutral-700" />
+          </div>
+          <div className="text-center text-neutral-600 text-xs font-normal font-['Pretendard']">
+            커뮤니티
+          </div>
+        </div>
+        <div className="flex-col justify-start items-center gap-1 inline-flex">
+          <div className="w-6 h-6 relative" />
+          <div className="text-center text-neutral-600 text-xs font-normal font-['Pretendard']">
+            마이
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default withDrawPage;
