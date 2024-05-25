40378f3dd32fa3629657d220be0b15272cfa4b93
jiohjung98 <ckaquz98@naver.com>
2024-05-25 19:05:52 +0900

    design: 오피스 상세 정보 페이지 레이아웃

---
+++ src/pages/officeInfo/index.tsx
@@ -0,0 +1,15 @@
+import MainContainer from '@/components/shared/MainContainer';
+
+const OfficePage = () => {
+  return (
+    <MainContainer>
+      <div className="flex flex-col justify-center items-center gap-[39px] h-screen">
+        <div className="w-full h-full">
+          
+        </div>
+      </div>
+    </MainContainer>
+  );
+};
+
+export default OfficePage;
