3761ca840bd1843fd2f2b8e41c790cff509cb5cc
jiohjung98 <ckaquz98@naver.com>
2024-05-26 18:37:33 +0900

    design: 오피스 상세페이지 디자인 작업

--- src/components/map/OfficeInfo.tsx
+++ src/components/map/OfficeInfo.tsx
@@ -5,6 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
+import { IoIosArrowRoundBack } from 'react-icons/io';
 
 interface OfficeInfoProps {
   branchName: string;
@@ -20,52 +21,98 @@ const OfficeInfo: React.FC<OfficeInfoProps> = ({ branchName, branchAddress }) =>
 
   return (
     <div className="relative w-full h-full">
-      <div className="absolute top-0 left-0 right-0 bg-white z-50 shadow-md p-4 flex items-center justify-between">
-        <button onClick={handleBackClick} className="text-lg font-bold">뒤로</button>
-        <span className="text-lg font-semibold">{branchName}</span>
+      <div className="absolute top-0 left-0 right-0 bg-white z-50 shadow-md py-4 flex items-center">
+        <IoIosArrowRoundBack size={40} className='ml-[6px]' onClick={handleBackClick}/>
+        <span className="text-lg font-semibold ml-[8px]">{branchName}</span>
         <div></div>
       </div>
       <div className="pt-16">
-        <Swiper
-          slidesPerView={1}
-          loop={true}
-          pagination={{ clickable: true }}
-          modules={[Pagination]}
-        >
-          <SwiperSlide>
-            <Image src="/OfficeDefaultImg.png" alt="Office Image 1" width={500} height={300} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/OfficeDefaultImg.png" alt="Office Image 2" width={500} height={300} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/OfficeDefaultImg.png" alt="Office Image 3" width={500} height={300} />
-          </SwiperSlide>
-        </Swiper>
-      </div>
-      <div className="p-4">
-        <h2 className="text-xl font-semibold">{branchName}</h2>
-        <p className="text-sm text-gray-600">{branchAddress}</p>
-        <div className="mt-4">
-          <h3 className="text-lg font-semibold">편의시설</h3>
-          <ul className="list-disc pl-5">
-            <li>라운지</li>
-            <li>리차징룸</li>
-            <li>무인택배</li>
-            <li>폰부스</li>
-            <li>복합기</li>
-            <li>무인 스낵바</li>
-            <li>사무용품</li>
-            <li>무제한 커피</li>
-          </ul>
+          <Swiper
+            slidesPerView={1}
+            loop={true}
+            pagination={{ clickable: true }}
+            modules={[Pagination]}
+          >
+            <SwiperSlide className="flex justify-center items-center h-full">
+              <Image src="/OfficeDefaultImg2.png" alt="Office Image 1" width={500} height={246} className="h-[246px] object-cover" />
+            </SwiperSlide>
+            <SwiperSlide className="flex justify-center items-center h-full">
+              <Image src="/OfficeDefaultImg2.png" alt="Office Image 2" width={500} height={246} className="h-[246px] object-cover" />
+            </SwiperSlide>
+            <SwiperSlide className="flex justify-center items-center h-full">
+              <Image src="/OfficeDefaultImg2.png" alt="Office Image 3" width={500} height={246} className="h-[246px] object-cover" />
+            </SwiperSlide>
+          </Swiper>
         </div>
-        <div className="mt-4">
-          <h3 className="text-lg font-semibold">공용 공간 리스트</h3>
+      <div className="">
+        <div className='p-4'>
+      <div className="text-black/opacity-20 text-lg font-extrabold py-[10px]">찾아오는길</div>
+        <div className='flex py-[10px]'>
+        <Image src="/OfficeLocationSmall1.svg" className='mb-auto pt-[3px]' alt="OfficeLocationSmall" width={12} height={16} />
+        <p className="text-sm text-gray-600 ml-[8px]">{branchAddress}</p>
+        </div>
+        <div className='flex py-[10px]'>
+        <Image src="/OfficeCallImg.svg" className='mb-auto pt-[3px]' alt="OfficeCallImg" width={12} height={16} />
+        <p className="text-sm text-gray-600 ml-[8px]">{branchAddress}</p>
+        </div>
+        <div className='flex py-[10px]'>
+        <Image src="/OfficeParkImg.svg" className='mb-auto pt-[3px]' alt="OfficeParkImg" width={12} height={16} />
+        <p className="text-sm text-gray-600 ml-[8px]">{branchAddress}</p>
+        </div> 
+        </div>
+        <div className="w-full h-1 bg-neutral-200" />
+        <div className="px-4 py-6">
+        <div className="text-black/opacity-20 text-lg font-extrabold py-[10px]">공용 공간 리스트</div>
           <ul>
             <li>A룸 - 10명 수용 가능</li>
             <li>B룸 - 9명 수용 가능</li>
             <li>C룸 - 13명 수용 가능</li>
           </ul>
+        </div>
+        <div className="w-full h-px bg-neutral-200" />
+        <div className="px-4 py-6">
+        <div className="text-black/opacity-20 text-lg font-extrabold py-[10px]">편의시설</div>
+        <div className="grid grid-cols-4 gap-4">
+          <div className="flex flex-col items-center py-[10px]">
+            <Image src="/LoungeImg.svg" alt="LoungeImg" width={12} height={16} className="w-[40px] h-[40px] my-auto"/>
+            <p className="mt-2">라운지</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image src="/RechargeImg.svg" alt="RechargeImg" width={12} height={16} className="w-[40px] h-[40px] my-auto"/>
+            <p className="mt-2">리차징룸</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image src="/ParcelImg.svg" alt="ParcelImg" width={12} height={16} className="w-[40px] h-[40px] my-auto"/>
+            <p className="mt-2">무인택배</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image src="/PhoneBooseImg.svg" alt="PhoneBooseImg" width={12} height={16} className="w-[40px] h-[40px my-auto"/>
+            <p className="mt-2">폰부스</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image src="/PrinterImg.svg" alt="PrinterImg" width={12} height={16} className="w-[40px] h-[40px my-auto"/>
+            <p className="mt-2">복합기</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image src="/SnackBarImg.svg" alt="SnackBarImg" width={12} height={16} className="w-[40px] h-[40px my-auto"/>
+            <p className="mt-2">스낵바</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image src="/SuppliesImg.svg" alt="SuppliesImg" width={12} height={16} className="w-[40px] h-[40px my-auto"/>
+            <p className="mt-2">사무용품</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image src="/CoffeeImg.svg" alt="CoffeeImg" width={12} height={16} className="w-[40px] h-[40px my-auto"/>
+            <p className="mt-2">무제한 커피</p>
+          </div>
+        </div>
+        </div>
+        <div className="w-full h-px bg-neutral-200" />
+        <div className="px-4 py-6">
+        <div className="text-black/opacity-20 text-lg font-extrabold py-[10px]">공지사항</div>
+        </div>
+        <div className='w-full text-center py-[30px]'>
+          <button className='w-[361px] h-12 bg-indigo-700 rounded-lg border border-indigo-700 text-center text-stone-50 text-[15px] font-semibold'>예약하기</button>
         </div>
       </div>
     </div>
