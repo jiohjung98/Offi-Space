347a335243a3a79b25fa514d177760b321a881a3
hojin <devbang5674@gmail.com>
2024-05-28 15:11:10 +0900

    fix: 커뮤니티 글 이미지 scale down

--- src/components/community/shared/PostItemImageItem.tsx
+++ src/components/community/shared/PostItemImageItem.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PostItemImageItem = ({ image, count }: { image: string; count: number }) => {
   return (
     <div className={`${count > 1 ? 'w-[260px] h-[180px] ' : 'w-[360px] h-[180px] z-1'}`}>
-      <img src={image} alt="" className="rounded-sm object-cover w-full h-full" />
+      <img src={image} alt="" className="rounded-sm object-scale-down w-full h-full" />
     </div>
   );
 };
