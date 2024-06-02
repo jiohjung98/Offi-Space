ad94d393d137a0dd859980b2868eabe7a191a160
hojin <devbang5674@gmail.com>
2024-06-03 01:36:01 +0900

    feat : 커뮤니티 페이지 글 상세조회 SSR 방식으로 전환

--- src/pages/community/[id]/index.tsx
+++ src/pages/community/[id]/index.tsx
@@ -1,7 +1,9 @@
-'use client';
 import PostDetailIndex from '@/components/community/PostDetailIndex';
 import MainContainer from '@/components/shared/MainContainer';
+import axios from 'axios';
+import { GetServerSidePropsContext } from 'next';
 import React from 'react';
+import { QueryClient, dehydrate } from 'react-query';
 
 const CommunityDetailPage = () => {
   return (
@@ -12,3 +14,31 @@ const CommunityDetailPage = () => {
 };
 
 export default CommunityDetailPage;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { query, req } = context;
+  const { cookie } = req.headers;
+  const postId = query.id as string;
+  const token = cookie ? cookie.split('=')[1] : '';
+
+  if (token !== '') {
+    const client = new QueryClient();
+    await client.prefetchQuery(['post', postId], async () => {
+      const { data } = await axios.get(`https://joo-api.store/posts/${postId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return data.data;
+    });
+    return {
+      props: {
+        dehydratedState: JSON.parse(JSON.stringify(dehydrate(client)))
+      }
+    };
+  }
+
+  return {
+    props: {}
+  };
+}
