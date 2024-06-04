34e9164f638ab7b9d2ec2b6676e3f99ecb698f76
jiohjung98 <ckaquz98@naver.com>
2024-06-04 22:58:44 +0900

    design: 미팅룸 상세정보 아이콘 추가

--- src/components/reservation/meetingRoom/MeetingRoomInfo.tsx
+++ src/components/reservation/meetingRoom/MeetingRoomInfo.tsx
@@ -97,12 +97,15 @@ const MeetingRoomInfo = () => {
                 <div className="flex flex-col w-full mt-[24px]">
                     <div className="text-black/opacity-20 text-lg font-bold font-['Pretendard']">{meetingRoom.meetingRoomName}</div>
                     <div className="flex flex-row items-center">
+                        <Image src={'/floor.svg'} width={14} height={14} alt="floor" className="mr-[6px]" />
                         <div className="text-stone-500 text-xs font-normal font-['Pretendard'] mr-[12px] mt-[2px]">
                             {meetingRoom.meetingRoomFloor}층
                         </div>
+                        <Image src={'/capacity.svg'} width={14} height={14} alt="capacity" className="mr-[6px]" />
                         <div className="text-stone-500 text-xs font-normal font-['Pretendard'] mr-[12px] mt-[2px]">
                             1~{meetingRoom.meetingRoomCapacity}명
                         </div>
+                        <Image src={'/check.svg'} width={14} height={14} alt="check" className="mr-[6px]" />
                         <div className="text-stone-500 text-xs font-normal font-['Pretendard'] mt-[2px]">
                             {meetingRoom.equipments.join(', ')}
                         </div>
